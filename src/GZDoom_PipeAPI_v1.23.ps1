98ceabe0e213a2977e1a69e4132e0f37501fd343
Jeremy Tiggy <jeremytiggy@gmail.com>
2026-08-22 00:40:04 -0700

    Refactor GZDoom pipe API connection functions

--- src/GZDoom_PipeAPI_v1.23.ps1
+++ src/GZDoom_PipeAPI_v1.23.ps1
@@ -96,46 +96,45 @@ $Global:NamedPipe_Client_AvailablePipeSelection_Filter = $true
 $Global:NamedPipe_Client_AutomaticallySelectUniqueFilteredPipeServerName = $true
 $Global:NamedPipe_Client_AvailablePipeSelection_NamePattern = '^[ZUG]ZD(_\d+)?$'
 $Global:NamedPipe_Server_Process = 'ZDoom'
-function GZDoom_PipeAPI_AutoConnect {
-	# enforce filtering & autoselection
-	$Global:NamedPipe_Server_Name = 'Select'
-	$Global:NamedPipe_Client_AvailablePipeSelection_Filter = $true
-	$Global:NamedPipe_Client_AutomaticallySelectUniqueFilteredPipeServerName = $true
-	$Global:NamedPipe_Client_AvailablePipeSelection_NamePattern = '^[ZUG]ZD(_\d+)?$'
-	$Global:NamedPipe_Server_Process = 'ZDoom'
-	# Check if one of the three supported processes is running
+function GZDoom_PipeAPI_Server_Process {
+# Check if one of the three supported processes is running
 	$numberOfKnownZDoomProcessesRunning = 0
-	$Server_Process = $Global:NamedPipe_Server_Process
-	$NamePattern = '^[ZUG]ZD(_\d+)?$'
 	# gzdoom
 	if (NamedPipe_Client_ProcessRunning "gzdoom") {
 		$numberOfKnownZDoomProcessesRunning += 1
 		$runningProcess = "gzdoom"
-		$NamePattern = '^[G]ZD(_\d+)?$'
 	}
 	# uzdoom
 	if (NamedPipe_Client_ProcessRunning "uzdoom") {
 		$numberOfKnownZDoomProcessesRunning += 1
 		$runningProcess = "uzdoom"
-		$NamePattern = '^[U]ZD(_\d+)?$'
 	}
 	#zandronum
 	if (NamedPipe_Client_ProcessRunning "zandronum") {
 		$numberOfKnownZDoomProcessesRunning += 1
 		$runningProcess = "zandronum"
-		$NamePattern = '^[Z]ZD(_\d+)?$'
 	}
-	# if one or more of them is running, 
-	if ($numberOfKnownZDoomProcessesRunning -ge 1) {
-		if ($numberOfKnownZDoomProcessesRunning -eq 1) {
-			Write-Host "[GZDoom_PipeAPI_Connect]: $($runningProcess) is running :-)" -ForegroundColor Green
-			$Global:NamedPipe_Server_Process = $runningProcess
-		} else if ($numberOfKnownZDoomProcessesRunning -ge 2) {
-			Write-Host "[GZDoom_PipeAPI_Connect]: $($runningProcess) multiple ports of DOOM are running O_o" -ForegroundColor Yellow 
-		}		
-	} else if ($numberOfKnownZDoomProcessesRunning -le 0) {
-		Write-Host "[GZDoom_PipeAPI_Connect]: $($runningProcess) no supported DOOM ports are running at the moment :(" -ForegroundColor Red
+
+	if ($numberOfKnownZDoomProcessesRunning -eq 1) {
+		Write-Host "[GZDoom_PipeAPI_Server_Process]: $($runningProcess) is running" -ForegroundColor Green
+		return $runningProcess
+	} else {
+		if ($numberOfKnownZDoomProcessesRunning -gt 1) {
+			Write-Host "[GZDoom_PipeAPI_NamedPipe_Client_Startup]: $($runningProcess) multiple ports of DOOM are running O_o" -ForegroundColor Yellow 
+		} else {
+			Write-Host "[GZDoom_PipeAPI_NamedPipe_Client_Startup]: $($runningProcess) no supported DOOM ports are running at the moment :(" -ForegroundColor Red
+		}
+		return "zdoom"
 	}
+}
+function GZDoom_PipeAPI_NamedPipe_Client_Startup {
+	# enforce filtering & autoselection
+	$Global:NamedPipe_Server_Name = 'Select'
+	$Global:NamedPipe_Client_AvailablePipeSelection_Filter = $true
+	$Global:NamedPipe_Client_AutomaticallySelectUniqueFilteredPipeServerName = $true
+	$Global:NamedPipe_Client_AvailablePipeSelection_NamePattern = '^[ZUG]ZD(_\d+)?$'
+	$Global:NamedPipe_Server_Process = GZDoom_PipeAPI_Server_Process
+	
 	NamedPipe_Client_Startup
 }
 
